a45a27cba299979300e478df1f055c35d5990f71
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2017-06-07 10:34:00 +0200

    Updated version in banner

--- src/util/exceptions/IllegalArgumentException.ts
+++ src/util/exceptions/IllegalArgumentException.ts
@@ -1,5 +1,5 @@
 /*!
-governify-agreement-analyzer 0.5.2, built on: 2017-06-07
+governify-agreement-analyzer 0.5.3, built on: 2017-06-07
 Copyright (C) 2017 ISA group
 http://www.isa.us.es/
 https://github.com/isa-group/governify-agreement-analyzer
