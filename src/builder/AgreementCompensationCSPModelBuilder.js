a686d4be954b495df6b496f79fff624353264e5f
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2017-10-17 12:21:28 +0200

    Updated usebanner version

--- src/builder/AgreementCompensationCSPModelBuilder.js
+++ src/builder/AgreementCompensationCSPModelBuilder.js
@@ -1,6 +1,6 @@
 "use strict";
 /*!
-governify-agreement-analyzer 0.6.2, built on: 2017-10-17
+governify-agreement-analyzer 0.6.3, built on: 2017-10-17
 Copyright (C) 2017 ISA group
 http://www.isa.us.es/
 https://github.com/isa-group/governify-agreement-analyzer
